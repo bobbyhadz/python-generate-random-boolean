93dcb2f5e5d8f4cddc3aa62ca79b573b2f0fce33
bobbyhadz <opmftw@gmail.com>
2024-04-10 13:13:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Generate a random Boolean using random.getrandbits()
+
+import random
+
+
+result = bool(random.getrandbits(1))
+print(result)  # 👉️ False
+
+result = bool(random.getrandbits(1))
+print(result)  # 👉️ True
